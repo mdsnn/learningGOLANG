22fa8007c128dff0d127c9224683464b3a70fe4d
mdsnn <mwikosinyangwe@gmail.com>
2026-05-03 18:04:35 +0200

    jjjj

--- main.go
+++ main.go
@@ -2,6 +2,22 @@ package main
 
 import "fmt"
 
+type User struct {
+	ID    int
+	Name  string
+	Email string
+}
+
+func (u User) DisplayName() string {
+	return u.Name + " <" + u.Email + ">"
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+	user := User{
+		ID:    1,
+		Name:  "Alex",
+		Email: "alex@example.com",
+	}
+
+	fmt.Println(user.DisplayName())
 }

5e8fad905121019b5d306b7971e95da63a1f0fa0
mdsnn <mwikosinyangwe@gmail.com>
2026-05-04 15:11:04 +0200

    ffff

--- main.go
+++ main.go
@@ -5,21 +5,34 @@ import "fmt"
 type Product struct {
 	ID      int
 	Name    string
-	Price   string
+	Price   float64
 	InStock bool
 }
 
 func (p Product) Label() string {
-	return p.Name + " - " + "$" + p.Price
+	return fmt.Sprintf("%s - $%.2f", p.Name, p.Price)
+}
+
+func (p *Product) MarkOutOfStock() {
+	p.InStock = false
+}
+func (p *Product) ApplyDiscount(percent float64) {
+	discount := p.Price * (percent / 100)
+	p.Price -= discount
 }
 
 func main() {
 	product := Product{
 		ID:      1,
-		Name:    "laptop",
-		Price:   "1200",
+		Name:    "Laptop",
+		Price:   1200,
 		InStock: true,
 	}
 
+	product.MarkOutOfStock()
+	product.ApplyDiscount(10)
+
 	fmt.Println(product.Label())
+	fmt.Println(product.InStock)
+
 }
